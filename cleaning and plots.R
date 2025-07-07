88fb119b7187c8974a486f637a6641a2fcd98e02
jacklardner <113477244+jacklardner@users.noreply.github.com>
2025-07-07 13:33:47 -0400

    adding openair visuals from DEM and modulair:

--- cleaning and plots.R
+++ cleaning and plots.R
@@ -5,7 +5,7 @@ library(lubridate)
 library(openair)
 library(ggplot2)
 
-no2_bysite_df <- read_csv("data/no2_calibrated.csv")
+no2_calibrated_bysite_df <- read_csv("data/no2_calibrated.csv")
 no2_long <- no2_bysite_df %>%
   pivot_longer(
     cols = -local_timestamp,
@@ -24,8 +24,15 @@ timeVariation(no2_long, pollutant = "no2", group = "site")
 
 
 ggplot(no2_long, aes(x = no2)) +
-  geom_histogram(binwidth = 1, fill = "steelblue", color = "black") +
+  geom_histogram(binwidth = 1, fill = "steelblue", color = "cyan") +
   facet_wrap(~site, scales = "free_y") +
-  labs(title = "NO2 Concentration Distributions by Site", x = "NO₂ (ppb)", y = "Count") +
+  labs(title = "NO2 Concentration Distributions by Site", x = "NO2 (ppb)", y = "Count") +
   theme_minimal()
+
+
+
+site_data <- no2_long %>% filter(site == "250_no2")
+timeVariation(site_data, pollutant = "no2", main = "Time Variation for Site 250")
+
+
 
