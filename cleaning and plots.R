ead4a2003242bccbf4d59a673f72d64fb4f417a4
jacklardner <113477244+jacklardner@users.noreply.github.com>
2025-08-05 13:25:29 -0400

    updating all code

--- cleaning and plots.R
+++ cleaning and plots.R
@@ -5,7 +5,9 @@ library(lubridate)
 library(openair)
 library(ggplot2)
 
-no2_calibrated_bysite_df <- read_csv("data/no2_calibrated.csv")
+no2_bysite_df <- read_csv("data/no2_calibrated.csv")
+ref_df <- read_csv("data/ep_no2_2023.csv")
+
 no2_long <- no2_bysite_df %>%
   pivot_longer(
     cols = -local_timestamp,
@@ -35,4 +37,45 @@ site_data <- no2_long %>% filter(site == "250_no2")
 timeVariation(site_data, pollutant = "no2", main = "Time Variation for Site 250")
 
 
+ref_df_clean <- ref_df %>%
+  mutate(
+    datetime = as.POSIXct(paste(date_local, time_local), format = "%Y-%m-%d %H:%M", tz = "America/New_York"),
+    no2 = sample_measurement
+  ) %>%
+  select(date = datetime, no2) %>%
+  filter(!is.na(no2))
 
+timeVariation(ref_df_clean, pollutant = "no2", main = "Time Variation - DEM")
+
+
+
+
+sensor_df <- read_csv("data/NO2_DPW_withFlatline.csv")
+
+sensor_df_clean <- sensor_df %>%
+  filter(flatline_flag == FALSE) %>%
+  mutate(
+    timestamp_parsed = as.POSIXct(timestamp_local, tz = "UTC"),
+    
+    date = with_tz(timestamp_parsed, tzone = "America/New_York"),
+    
+    hour = floor_date(date, unit = "hour")
+  )
+
+#average on the hour
+sensor_hourly <- sensor_df_clean %>%
+  group_by(hour) %>%
+  summarise(no2 = mean(no2, na.rm = TRUE)) %>%
+  ungroup() %>%
+  rename(date = hour)
+
+timeVariation(sensor_hourly, pollutant = "no2", main = "Hourly-Averaged NO2 - MOD-00811")
+
+ggplot(sensor_hourly, aes(x = no2)) +
+  geom_histogram(binwidth = 1, fill = "steelblue", color = "white", boundary = 0) +
+  labs(
+    title = "Histogram of Hourly-Averaged NO2 - MOD-00811",
+    x = "NO2 (ppb)",
+    y = "Count"
+  ) +
+  theme_minimal()
